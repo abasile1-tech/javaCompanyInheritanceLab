ccc586acfc9e155635681f0e288b7aee64034537
John <ashton.basile@gmail.com>
2023-09-20 15:25:31 +0100

    I wrote the tests for the Manager class as part of TDD

--- src/main/java/management/Manager.java
+++ src/main/java/management/Manager.java
@@ -2,5 +2,11 @@ package management;
 
 import staff.Employee;
 
+//#### Create a subclass of `Employee` called `Manager` in a package called management.
+//        - Create a class for Manager.
+//        - Add a property to store the department name in a property called `deptName`.
+//        - Create a constructor that includes all the parameters needed for `Employee` and `deptName`.
+//        - Add a getter method for `deptName`.
+//        - Test all methods including `raiseSalary` and `payBonus`
 public class Manager extends Employee {
 }
--- src/test/java/ManagerTest.java
+++ src/test/java/ManagerTest.java
@@ -1,5 +1,29 @@
+import management.Manager;
 import org.junit.Test;
 import org.junit.Before;
 import static org.junit.Assert.assertEquals;
+
+//#### Create a subclass of `Employee` called `Manager` in a package called management.
+//        - Create a class for Manager.
+//        - Add a property to store the department name in a property called `deptName`.
+//        - Create a constructor that includes all the parameters needed for `Employee` and `deptName`.
+//        - Add a getter method for `deptName`.
+//        - Test all methods including `raiseSalary` and `payBonus`
 public class ManagerTest {
+    private Manager manager;
+    @Before
+    public void before() {
+        manager = new Manager("John", 2, 40_000);
+    }
+    @Test
+    public void canRaiseSalary() {
+        manager.raiseSalary(2000);
+        assertEquals(42000, manager.getSalary());
+    }
+
+    @Test
+    public void canPayBonus() {
+        manager.payBonus();
+        assertEquals(40400, manager.getSalary());
+    }
 }
