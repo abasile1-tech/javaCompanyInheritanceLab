b3441502bf159fc9b7eb53c311ad2e29aada54fe
John <ashton.basile@gmail.com>
2023-09-20 15:18:40 +0100

    I wrote the Employee abstract class

--- src/main/java/staff/Employee.java
+++ src/main/java/staff/Employee.java
@@ -1,4 +1,33 @@
 package staff;
 
 public abstract class Employee {
+    private String name;
+    private int ni;
+    private double salary;
+
+    public Employee(String name, int ni, double salary) {
+        this.name = name;
+        this.ni = ni;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNi() {
+        return ni;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void raiseSalary(double amount) {
+        this.salary += amount;
+    }
+
+    public double payBonus() {
+        return this.salary * 0.01;
+    }
 }
