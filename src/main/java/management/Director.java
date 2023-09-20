8f34aa39e74e2c1f3f106a93bb0eb750b411eb65
John <ashton.basile@gmail.com>
2023-09-20 16:05:57 +0100

    We wrote the Director class and tests

--- src/main/java/management/Director.java
+++ src/main/java/management/Director.java
@@ -1,5 +1,14 @@
-//package management;
-//
-//public class Director extends Manager{
-//
-//}
+package management;
+
+public class Director extends Manager{
+    private double budget;
+
+    public Director(String name, int ni, double salary, String deptName, double budget) {
+        super(name, ni, salary, deptName);
+        this.budget = budget;
+    }
+
+    public double getBudget() {
+        return budget;
+    }
+}
--- src/test/java/DirectorTest.java
+++ src/test/java/DirectorTest.java
@@ -1,5 +1,27 @@
+import management.Director;
+import management.Manager;
 import org.junit.Test;
 import org.junit.Before;
 import static org.junit.Assert.assertEquals;
 public class DirectorTest {
+    private Director director;
+    @Before
+    public void before() {
+        director = new Director("John", 2, 40_000, "Software", 3000);
+    }
+    @Test
+    public void canRaiseSalary() {
+        director.raiseSalary(2000);
+        assertEquals(42000, director.getSalary(), 0.0);
+    }
+
+    @Test
+    public void canPayBonus() {
+        assertEquals(400, director.payBonus(), 0.0);
+    }
+
+    @Test
+    public void canGetBudget() {
+        assertEquals(3000, director.getBudget(), 0.0);
+    }
 }
